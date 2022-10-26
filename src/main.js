f90fb1d3763053af7307fe4df4949b68c4383314
凌寒舞 <2273376391@qq.com>
2022-10-26 16:37:12 +0800

    状态管理

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // 导入路由
 import router from './router'
+// 状态管理
+import pinia from './stores'
+
+
 // 引入样式文件
 import './assets/css/index.css'
 
+const app = createApp(App)
 
-
-createApp(App).use(router).mount('#app')
+app.use(router)
+app.use(pinia)
+app.mount('#app')
