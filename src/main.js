2c375ce1bafc84e8f87968c2384b28c35093e261
凌寒舞 <2273376391@qq.com>
2022-10-26 16:06:54 +0800

    样式重置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 引入样式重置文件
+import 'normalize.css'
+import './assets/css/reset.css'
 
-import './assets/main.css'
 
 createApp(App).mount('#app')
