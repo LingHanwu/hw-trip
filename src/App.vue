1f5ce3125db1ceb821329c725da9dc1920957be8
凌寒舞 <2273376391@qq.com>
2022-10-26 19:25:02 +0800

    基础页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
-
+import TabBar from '@/components/tab-bar/tab-bar.vue'
 </script>
 
 <template>
   <div class="app">
-    <router-link to="/home">首页</router-link>
-    <router-link to="/favor">收藏</router-link>
-    <router-link to="/order">订单</router-link>
-    <router-link to="/message">消息</router-link>
+    <TabBar></TabBar>
     <router-view></router-view>
   </div>
 </template>
---
+++ src/components/tab-bar/tab-bar.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="tab-bar">
+    <div class="tab-bar-item">
+      <span>首页</span>
+    </div>
+    <div class="tab-bar-item">收藏</div>
+    <div class="tab-bar-item">订单</div>
+    <div class="tab-bar-item">消息</div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  display: flex;
+
+  border-top: 1px solid orange;
+
+  .tab-bar-item {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
