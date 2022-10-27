7102c60af6c2a9bc957f8d4647b08e5a5d0611a5
凌寒舞 <2273376391@qq.com>
2022-10-27 10:06:54 +0800

    home导航栏

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,56 @@
 <script setup>
-
+import homeNarBar from './navBar/homeNavBar.vue'
 </script>
 
 <template>
   <div class="home">
-    <h2>home</h2>
+    <!-- 导航栏 -->
+    <homeNarBar></homeNarBar>
+    <div class="banner">
+      <img src="@/assets/img/home/banner.webp" alt="banner">
+    </div>
+
+
+    <div class="location">
+      <div class="city">广州</div>
+      <div class="position">
+        <span class="text">我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="位置图片">
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style lang="less" scoped>
+.banner {
+  img {
+    width: 100%;
+  }
+}
 
+.location {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  padding: 0 20px;
+
+  .city {
+    flex: 1;
+  }
+
+  .position {
+    width: 74px;
+
+    .text {
+      font-size: 12px;
+    }
+
+    img {
+      margin-left: 5px;
+      width: 18px;
+      height: 18px;
+    }
+  }
+}
 </style>
---
+++ src/views/home/navBar/homeNavBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="navBar">
+    <!-- varlet导航栏  https://varlet.gitee.io/varlet-ui/#/zh-CN/app-bar -->
+    <var-app-bar title="标题" title-position="center" color="#00c48f" />
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
